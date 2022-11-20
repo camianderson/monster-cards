22873c15a264df566f87a285a9f5b789811ba5ce
Cami Anderson <camidantas@icloud.com>
2022-11-20 11:17:30 -0500

    Add basic sturcture for CardList

---
+++ src/components/CardList/CardList.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class CardList extends Component {
+
+    render(){
+        return (
+            <h1>Hi</h1>
+        )
+    }
+}
+
+export default CardList;
