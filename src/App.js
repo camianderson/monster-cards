15daa4aecceb2b6d3d717687f643ece93037c434
Cami Anderson <camidantas@icloud.com>
2022-11-20 10:48:25 -0500

    Add searchField in state and modify search functionality

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
-    }
+      monsters: [],
+      searchField: ''
+    };
   }
 
   componentDidMount(){
@@ -19,10 +20,15 @@ class App extends Component {
         () => {
           return { monsters: users }
         }
-      ))
+      ));
   }
 
   render(){
+
+    const filteredMonsters = this.state.monsters.filter(monster => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+    }); 
+
     return (
       <div className="App">
         <input 
@@ -30,19 +36,15 @@ class App extends Component {
           type='search'
           placeholder='search monster'
           onChange={event => {
-            const searchString = event.target.value.toLocaleLowerCase();
-            const filteredMonsters = this.state.monsters.filter(monster => {
-              return monster.name.toLocaleLowerCase().includes(searchString)
-            })
+            const searchField = event.target.value.toLocaleLowerCase();
             this.setState(
               () => {
-
-                return {monsters: filteredMonsters}
+                return {searchField};
               }
             )
           }}
         />
-       {this.state.monsters.map(monster => {
+       {filteredMonsters.map(monster => {
         return (
           <div key={monster.id}>
             <h1>{monster.name}</h1>
